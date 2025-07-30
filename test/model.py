e4af384187f50ecaa07ac4eb80069396d439de79
Laney Black PC <laney.black.74@gmail.com>
2025-07-30 17:11:55 +0200

    tranformers test

--- test/model.py
+++ test/model.py
@@ -1,4 +1,5 @@
 import torch
 print(torch.__version__)  # Should print 2.6.0 or higher
-from transformers import Wav2Vec2ForCTC
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 model = Wav2Vec2ForCTC.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-polish")
+processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-polish")
