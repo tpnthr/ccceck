75ca84bdf33da23cf07eebde31db4199a09b543d
Laney Black PC <laney.black.74@gmail.com>
2025-08-22 16:03:30 +0200

    Logs endpoints done

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
+import os
 from contextlib import asynccontextmanager
 
-import torch
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 from config import APP_NAME, VERSION, DEVICE, WHISPER_MODEL, ALLOW_SHUTDOWN
 from utils import logger
@@ -71,7 +71,6 @@ def health():
     return {"success": True, "device": DEVICE, "model": WHISPER_MODEL}
 
 
-
 @app.post("/shutdown")
 async def shutdown():
     if not ALLOW_SHUTDOWN:
@@ -86,16 +85,56 @@ async def shutdown():
     threading.Timer(1, exit_process).start()
     return {"success": True, "message": "Service shutting down"}
 
+
+@app.get("/log/{log_filename}", response_class=PlainTextResponse)
+async def view_log(log_filename: str):
+    """
+    Returns the content of a log file as plain text.
+    """
+    log_path = os.path.join("logs", f"{log_filename}.log")
+
+    # Ensure the file exists and is inside the LOG_DIR
+    if not os.path.isfile(log_path):
+        raise HTTPException(status_code=404, detail="Log file not found")
+
+    with open(log_path, "r", encoding="utf-8") as file:
+        log_content = file.read()
+
+    return PlainTextResponse(content=log_content)
+
+
+@app.delete("/log/{log_filename}")
+async def clear_log(log_filename: str):
+    """
+    Clears the content of a specified log file.
+    """
+    log_path = os.path.join("logs", f"{log_filename}.log")
+
+    # Ensure the file exists
+    if not os.path.isfile(log_path):
+        raise HTTPException(status_code=404, detail="Log file not found")
+
+    # Clear the log file
+    with open(log_path, "w", encoding="utf-8") as file:
+        file.write("")
+
+    return {
+        "success": True,
+        "detail": f"Log file '{log_filename}.log' has been cleared.",
+    }
+
+
 from routes.stereo import router as stereo_router
 
 app.include_router(stereo_router, prefix="/stereo", tags=["Stereo"])
 
 from routes.mono import router as mono_router
+
 app.include_router(mono_router, prefix="/mono", tags=["Mono"])
 
 from routes.file import router as file_router
-app.include_router(file_router, prefix="/file", tags=["File"])
 
+app.include_router(file_router, prefix="/file", tags=["File"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
